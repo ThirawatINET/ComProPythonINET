449ff0f051ec41b547c5b5c29bdfcd099c41e811
ThirawatINET <s6806022510357@email.kmutnb.ac.th>
2025-06-26 16:37:20 +0700

    FirstCommit

---
+++ Lecture-01/testprint.py
@@ -0,0 +1,9 @@
+print('Hello World')
+print('Thirawat Phothisawang')
+print("I'm hungry")
+print("I'm good and \"I will do my best\"")
+print("/\\")
+#print("\ta\na\ta\ta\n\ta")
+print('''\ta
+a\ta\ta
+\ta''')
